b1034b4353562accfcffefed590f23b778d1999e
krzys-h <krzys_h@interia.pl>
2018-01-13 13:02:13 +0100

    User sync

--- app/src/main/java/net/elfdump/campusassistant/MainActivity.java
+++ app/src/main/java/net/elfdump/campusassistant/MainActivity.java
@@ -1,6 +1,8 @@
 package net.elfdump.campusassistant;
 
+import android.graphics.Color;
 import android.os.Debug;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -15,56 +17,79 @@ import com.indoorway.android.common.sdk.model.VisitorLocation;
 import com.indoorway.android.common.sdk.task.IndoorwayTask;
 import com.indoorway.android.fragments.sdk.map.IndoorwayMapFragment;
 import com.indoorway.android.fragments.sdk.map.MapFragment;
+import com.indoorway.android.map.sdk.view.drawable.figures.DrawableCircle;
+import com.indoorway.android.map.sdk.view.drawable.figures.DrawableIcon;
 import com.indoorway.android.map.sdk.view.drawable.figures.DrawableText;
 import com.indoorway.android.map.sdk.view.drawable.layers.MarkersLayer;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Date;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements IndoorwayMapFragment.OnMapFragmentReadyListener {
+    private final Handler mHandler = new Handler();
+    private MapFragment mapFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
+    private final Runnable mUpdateUI = new Runnable() {
+        public void run() {
+            IndoorwaySdk.instance()
+                .visitors()
+                .locations()
+                .setOnCompletedListener(new Action1<List<VisitorLocation>>() {
+                    @Override
+                    public void onAction(List<VisitorLocation> visitorLocations) {
+                        MarkersLayer myLayer = mapFragment.getMapView().getMarker().addLayer(100.0f);
+
+                        Log.i("DDFSFSDF", "WORKING");
+                        for(VisitorLocation visitor : visitorLocations) {
+                            if (visitor.getLat() == null || visitor.getLon() == null || visitor.getTimestamp() == null) continue; // DLACZEGO TE NULLE
+
+                            myLayer.remove(visitor.getVisitorUuid());
+
+                            if (new Date().getTime() - visitor.getTimestamp().getTime() > 10000) continue; // za stare
+
+                            Log.i("ASDDSFDSFSFS", visitor.toString());
+                            myLayer.add(
+                                new DrawableCircle(
+                                    visitor.getVisitorUuid(),
+                                    .5f, // radius in meters, eg. 0.4f
+                                    Color.RED, // circle background color, eg. Color.RED
+                                    Color.BLUE, // color of outline, eg. Color.BLUE
+                                    .1f, // width of outline in meters, eg. 0.1f
+                                    new Coordinates(visitor.getLat(), visitor.getLon()) // coordinates of circle center point
+                                )
+                            );
+                        }
+
+                        mHandler.postDelayed(mUpdateUI, 1000);
+                    }
+                })
+                .setOnFailedListener(new Action1<IndoorwayTask.ProcessingException>() {
+                    @Override
+                    public void onAction(IndoorwayTask.ProcessingException e) {
+                        Log.e("DFSDFSDDFS", "lel");
+                        mHandler.postDelayed(mUpdateUI, 1000);
+                    }
+                })
+                .execute();
+        }
+    };
+
     @Override
-    public void onMapFragmentReady(@NotNull final MapFragment mapFragment) {
+    public void onMapFragmentReady(@NotNull MapFragment mapFragment) {
+        this.mapFragment = mapFragment;
         mapFragment.getMapView().load(IndoorwayConstants.BUILDING_UUID, IndoorwayConstants.FLOOR2_UUID);
         mapFragment.getMapView().setOnMapLoadCompletedListener(new Action1<IndoorwayMap>() {
             @Override
             public void onAction(IndoorwayMap indoorwayMap) {
-                IndoorwaySdk.instance()
-                    .visitors()
-                    .locations()
-                    .setOnCompletedListener(new Action1<List<VisitorLocation>>() {
-                        @Override
-                        public void onAction(List<VisitorLocation> visitorLocations) {
-                            MarkersLayer myLayer = mapFragment.getMapView().getMarker().addLayer(100.0f);
-
-                            Log.i("DDFSFSDF", "WORKING");
-                            for(VisitorLocation visitor : visitorLocations) {
-                                Log.i("ASDDSFDSFSFS", visitor.toString());
-
-                                myLayer.add(
-                                    new DrawableText(
-                                        visitor.getVisitorUuid(),
-                                        new Coordinates(visitor.getLat(), visitor.getLon()),
-                                        visitor.getVisitorUuid(),
-                                        1.5f
-                                    )
-                                );
-                            }
-                        }
-                    })
-                    .setOnFailedListener(new Action1<IndoorwayTask.ProcessingException>() {
-                        @Override
-                        public void onAction(IndoorwayTask.ProcessingException e) {
-                            Log.e("DFSDFSDDFS", "lel");
-                        }
-                    })
-                    .execute();
+                mHandler.post(mUpdateUI);
             }
         });
     }
