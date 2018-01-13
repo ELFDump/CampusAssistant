5dc2a49eae86680c4a7dd9e080088cec96d23a6a
Adikso <adam@zambrzycki.net>
2018-01-13 16:43:43 +0100

    Add user get

--- app/src/main/java/net/elfdump/campusassistant/api/service/UserService.java
+++ app/src/main/java/net/elfdump/campusassistant/api/service/UserService.java
@@ -4,11 +4,16 @@ import net.elfdump.campusassistant.api.model.User;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 public interface UserService {
 
     @POST("users/create")
     Call<User> create(@Body User user);
+
+    @GET("users/{uuid}")
+    Call<User> get(@Path("uuid") String uuid);
 
 }
