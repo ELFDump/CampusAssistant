06b47b65d3ed45811fe90e0dd5bbe28d8d3b49ed
krzys-h <krzys_h@interia.pl>
2018-01-13 11:30:51 +0100

    Add API keys (yes, on GitHub :P)

---
+++ app/src/main/java/net/elfdump/campusassistant/IndoorwayConstants.java
@@ -0,0 +1,9 @@
+package net.elfdump.campusassistant;
+
+public final class IndoorwayConstants {
+    public static final String API_KEY = "72e6a109-b1d5-41cd-832c-3847a6716152";
+    public static final String BUILDING_UUID = "CScrSxCVhQg";
+    public static final String FLOOR0_UUID = "7-QLYjkafkE";
+    public static final String FLOOR1_UUID = "gVI7XXuBFCQ";
+    public static final String FLOOR2_UUID = "3-_M01M3r5w";
+}
